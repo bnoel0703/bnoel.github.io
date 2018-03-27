5fbb21f900d8e05a92a30dc5757fe3398868aa49
bnoel0703 <32888299+bnoel0703@users.noreply.github.com>
2018-03-26 20:33:42 -0400

    Added Images and began script

--- Script.js
+++ Script.js
@@ -1,1 +1,8 @@
 // JavaScript Document
+$(document).ready(function() {
+  $('.column-icon').animate({opacity: 1}, 2000);
+});
+
+$('modal-button').on("click", (function() {
+  $('.slidingAlert').delay(3000).slideUp(400);
+}));
