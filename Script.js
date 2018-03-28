feb45e0df55fa844ec000c0935f60739f7db7677
bnoel0703 <32888299+bnoel0703@users.noreply.github.com>
2018-03-28 19:07:47 -0400

    Minor Edits. Add more flair

--- Script.js
+++ Script.js
@@ -3,6 +3,6 @@ $(document).ready(function() {
   $('.column-icon').animate({opacity: 1}, 2000);
 });
 
-$('modal-button').on("click", (function() {
+$('modal-button').click(function() {
   $('.slidingAlert').delay(3000).slideUp(400);
-}));
+});
